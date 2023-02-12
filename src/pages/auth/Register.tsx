f93b1ac9dea732ab17b8fe7ebd4b1a2ff7b74083
Sukanta Das <arnobh84@gmail.com>
2023-02-12 15:05:26 +0600

    register page from added

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,7 +1,28 @@
 const Register = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const form = e.target;
+    const username = form.username.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user = { username, email, password };
+
+    fetch();
+  };
   return (
     <div>
       <p>Register</p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="username" placeholder="username" /> <br />{" "}
+          <br />
+          <input type="email" name="email" placeholder="email" /> <br /> <br />
+          <input type="password" name="password" placeholder="password" />{" "}
+          <br />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-
 const Login = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div>
-      <p>login</p>
+      <p>Login</p>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="email" /> <br /> <br />
+        <input type="password" name="password" placeholder="password" /> <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
